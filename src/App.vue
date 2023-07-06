ddf9bd4499c188d9d3722de70887d30c0ab9d807
MonicaIzzo <info@monicaizzo.net>
2023-07-06 06:45:00 +0200

    add components AppCard

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-/*
+/* MILESTONE 1
 Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
 Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
 
@@ -15,14 +15,23 @@ Bonus:
 - Importare il font utilizzando Fontsource
 */
 
+/* MILESTONE 2
+Continuate a lavorare nella stessa repo di ieri e create un nuovo componente che rappresenterà le card dei fumetti.
+Utilizzate i dati presenti nel file che trovate in allegato copiandoli nel vostro AppMain e passateli al componente ComicCard tramite props.
+Una volta inseriti tutti i contenuti dinamicamente, completate il vostro layout e rifinite i dettagli con Sass.
+Bonus:
+Far partire i dati dal componente App anziché dal componente AppMain !
+*/
+
+
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppHero from './components/AppHero.vue';
-import AppMain from './components/AppMain.vue';
+import AppCard from './components/AppCard.vue';
 import AppFooter from './components/AppFooter.vue';
 export default {
   name: 'Applicazione VITE COMICS',
-  components: { AppHeader, AppHero, AppMain, AppFooter },
+  components: { AppHeader, AppHero, AppCard, AppFooter },
   data() {
     return {}
   }
@@ -33,7 +42,7 @@ export default {
 <template>
   <AppHeader />
   <AppHero />
-  <AppMain />
+  <AppCard />
   <AppFooter />
 </template>
 
